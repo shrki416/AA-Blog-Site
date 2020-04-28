79ab42febf6ff801f46b8e15ce04ec942762d910
Ahmed Abdelaal <coach-ahmed@bmsnj.org>
2020-04-28 11:40:32 -0400

    roll back style change

--- src/components/layout.js
+++ src/components/layout.js
@@ -30,7 +30,7 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 1000,
+          maxWidth: 960,
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
